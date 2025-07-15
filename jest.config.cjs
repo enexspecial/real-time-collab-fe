d630feebc0bacc4233ddffcb9d9248852b82980e
enexspecial <john04star@gmail.com>
2025-07-15 19:05:01 +0100

    fix: remove jess

--- jest.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.[jt]sx?$': 'babel-jest',
-  },
-  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
-  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
-}; 
